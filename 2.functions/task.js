3652ba238809b49137aba33f1a4b9b7432788505
PhoenixReid <168421381+PhoenixReid@users.noreply.github.com>
2024-11-25 18:36:53 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,21 @@
 function getArrayParams(...arr) {
-
+let min = 0
+    let max = 0
+    let avg = 0
+    for (let i = 0; i < arr.length; i++)
+        {
+        if(arr[i] < min){
+            min = arr[i];
+        }
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        avg = avg + arr[i];
+    }
+    avg = (avg / arr.length);
+    avg = avg.toFixed(2);
+    avg -parseFloat(avg)
+    return { min: min, max: max, avg: avg };
   return { min: min, max: max, avg: avg };
 }
 
