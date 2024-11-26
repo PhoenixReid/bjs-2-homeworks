f66f5a40d1e34e6ef91aa84caac3dddd26403159
PhoenixReid <168421381+PhoenixReid@users.noreply.github.com>
2024-11-26 14:05:50 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -5,7 +5,6 @@ function compareArrays(arr1, arr2) {
       
       return arr1.every((element, index) => element === arr2[index]);
     }
-}
 
 function getUsersNamesInAgeRange(users, gender) {
    const userGender = users.filter(users => users.gender == gender)
