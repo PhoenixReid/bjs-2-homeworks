84b9205c4ac1b43e1babbee15a773e06f3f0d87a
PhoenixReid <168421381+PhoenixReid@users.noreply.github.com>
2024-11-28 17:44:34 +0300

    Update task.js

---
+++ 5.classes/task.js
@@ -0,0 +1,122 @@
+class PrintEditionItem{
+  constructor(name, releaseDate, pagesCount){
+    this.name = name
+    this.releaseDate = releaseDate
+    this.pagesCount = pagesCount
+    this.state = 100
+    this.type = null
+  }
+
+  fix(){
+    if(this.state == 100){
+      console.log("Книга новая и не подлежит восстановлению")
+      return
+    }
+    if(this.state == 0){
+      console.log("Невозможно востановить книгу")
+      return
+    }
+    this.state = this.state*1.5
+    if(this.state > 100){
+      this.state = 100
+    }
+    return this.state
+  }
+
+  setState(state){
+    if (state<0){
+      this.state = 0
+      return this.state
+    }
+    if(state > 100){
+      this.state = 100
+      return this.state
+    }
+    this.state = state
+    return this.state
+  }
+
+  getState(){
+    return this.state
+  }
+}
+
+class Library{
+  constructor(name){
+  this.name = name
+  this.books = []
+  }
+
+  addBook(book){
+    if (book.state > 30){
+      this.books.push(book)
+    }
+  }
+
+  findBookBy(type, value) {
+    const book = this.books.find(book => book[type] === value);
+    return book ? book : null;
+}
+
+giveBookByName(bookName){
+  const delBook = this.findBookBy("name", bookName)
+  this.books = this.books.filter(book => book !== delBook)
+}
+}
+
+class Magazine extends PrintEditionItem{
+  constructor(name,releaseDate, pagesCount){
+    super(name, releaseDate, pagesCount)
+    this.type = "Magazine"
+  }
+
+}
+
+class Book extends PrintEditionItem{
+  constructor(author, name, releaseDate, pagesCount){
+    super(name, releaseDate, pagesCount)
+    this.author = author
+    this.type = "Book"
+  }
+
+}
+
+class FantasticBook extends Book{
+  constructor(author, name, releaseDate, pagesCount){
+    super(author, name, releaseDate, pagesCount)
+    this.type = "fantastic"
+  }
+}
+
+class NovelBook  extends Book{
+  constructor(author, name, releaseDate, pagesCount){
+    super(author, name, releaseDate, pagesCount)
+    this.type = "novel"
+  }
+}
+
+class DetectiveBook  extends Book{
+  constructor(author, name, releaseDate, pagesCount){
+    super(author, name, releaseDate, pagesCount)
+    this.type = "detective"
+  }
+}
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+ new DetectiveBook(
+   "Артур Конан Дойл",
+   "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+   2019,
+   1008
+ )
+);
+library.addBook(
+ new FantasticBook(
+   "Аркадий и Борис Стругацкие",
+   "Пикник на обочине",
+   1972,
+   168
+ )
+);
